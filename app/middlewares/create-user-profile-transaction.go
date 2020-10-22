995b487f1be353939e172765e8e7dcdcea9cdb50
unknown <vahnxp@gmail.com>
2020-10-22 18:50:46 -0300

    fix transaction creation method

--- app/middlewares/create-user-profile-transaction.go
+++ app/middlewares/create-user-profile-transaction.go
@@ -2,29 +2,30 @@ package middlewares
 
 import (
 	"net/http"
-	"os"
 
 	commonModels "github.com/Tsuryu/arreglapp-commons/app/models"
+	"github.com/Tsuryu/arreglapp-commons/app/service"
 	"github.com/Tsuryu/arreglapp-user-profile/app/models"
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
-	"github.com/go-resty/resty/v2"
 )
 
 // CreateUserProfileTransaction : init transaction
 func CreateUserProfileTransaction(context *gin.Context) {
 	userProfile := models.UserProfile{}
 	context.ShouldBindBodyWith(&userProfile, binding.JSON)
+
 	transaction := commonModels.Transaction{}
-	client := resty.New()
+	transactionDetail := commonModels.TransactionDetail{}
 
-	request := client.R()
-	detail := `{"metadata":{"step": "init enrollment"}}`
-	request.SetBody(`{"reference":"create user profile", "email": "` + userProfile.Email + `", "details":[` + detail + `]}`)
-	request.SetHeader("with-security-code", "true")
-	request.SetResult(&transaction)
+	transactionDetail.Metadata = struct {
+		Step string `json:"step"`
+	}{"init enrollment"}
+	transaction.Reference = "create user profile"
+	transaction.Email = userProfile.Email
+	transaction.Details = append(transaction.Details, transactionDetail)
 
-	_, err := request.Post(os.Getenv("API_TRANSACTION_BASE_URL") + "/transaction")
+	err := service.NewTransaction(&transaction, true)
 	if err != nil {
 		context.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": "Failed to init transaction"})
 		return
